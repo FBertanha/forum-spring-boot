3d1cf566fc3a75db25178129380abb485582883c
Felipe Bertanha <bertanhahp@gmail.com>
2019-06-24 22:58:24 -0300

    Aula 3 spring data jpa, repository, filtros

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,29 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
-    @RequestMapping("/topicos")
-    public List<TopicoDto> lista() {
-        Topico topico = new Topico("Titulo srsrsrsr", "Mensagem 1", new Curso("nome do curso 1", "categoria do curso 1"));
+    @Autowired
+    private TopicoRepository topicoRepository;
 
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+
+        } else {
+            List<Topico> cursos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(cursos);
+        }
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Usuario(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+
+INSERT INTO Curso(nome, categoria) VALUES('Spring Boot', 'Programacão');
+INSERT INTO Curso(nome, categoria) VALUES('HTML 5', 'Front-end');
+
+
+INSERT INTO Topico(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES ('Dúvida 1', 'Mensagem 1', '2019-01-01 01:01:01', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO Topico(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES ('Dúvida 1', 'Mensagem 1', '2019-01-01 01:01:01', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO Topico(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES ('Dúvida 1', 'Mensagem 1', '2019-01-01 01:01:01', 'NAO_RESPONDIDO', 1, 1);
