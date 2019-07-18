585f317502aeb698ec89960fffdccea71fb5f873
Felipe Bertanha <bertanhahp@gmail.com>
2019-07-18 00:01:58 -0300

    Parte 2
    Aula 3
    -Spring security, user and password, bcrypt. UserDetails, UserDetailsService

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Usuario(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO Usuario(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$8NDfHPPbaqTzCWuAsqXCjeW0rdH79UssDWhbfygwgXpVaWodgO/ZO');
 
 INSERT INTO Curso(nome, categoria) VALUES('Spring Boot', 'Programacão');
 INSERT INTO Curso(nome, categoria) VALUES('HTML 5', 'Front-end');
